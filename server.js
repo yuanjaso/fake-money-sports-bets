d34ac6139b6ce08a90ba96dbd8e9c15cb9d41db4
Jason Yuan <yuanjason98@gmail.com>
2020-12-30 18:08:30 -0500

    added helmet package for security middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 // environment variables
 const PORT = process.env.PORT;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.static(`${__dirname}/dist/fake-money-sports-bets/`));
 app.use(morgan('dev'));
+app.use(helmet());
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
