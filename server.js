3658e79a42d65bf04d015abcd76fe15e078ea330
Jason Yuan <yuanjason98@gmail.com>
2020-12-30 18:15:37 -0500

    added body-parser for json req bodies

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 
 // environment variables
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.static(`${__dirname}/dist/fake-money-sports-bets/`));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(helmet());
 
