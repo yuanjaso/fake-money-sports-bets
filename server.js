414b29308e3e05fa6fc9b601267449f9740326ba
Jason Yuan <yuanjason98@gmail.com>
2020-12-30 18:00:39 -0500

    added setting to serve frontend static files

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(express.static(`${__dirname}/dist/fake-money-sports-bets/`));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
