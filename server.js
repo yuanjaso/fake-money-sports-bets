0476dd3811d89c0f8297cd86e58843c1f6961c23
Jason Yuan <yuanjason98@gmail.com>
2020-12-30 18:06:43 -0500

    added morgan for logging

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // environment variables
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.static(`${__dirname}/dist/fake-money-sports-bets/`));
+app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
