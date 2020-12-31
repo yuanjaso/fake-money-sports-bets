bdae96efb5b5016f22d1584235ed229debae1067
Jason Yuan <yuanjason98@gmail.com>
2020-12-31 14:07:40 -0500

    added auth comp

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthComponent],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
