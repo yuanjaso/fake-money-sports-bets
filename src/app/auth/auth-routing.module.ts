f22b5d303f58ec31773720d338c46b7f41e4b2c9
Jason Yuan <yuanjason98@gmail.com>
2020-12-31 14:26:11 -0500

    connect signup/signin comp to auth comp

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthComponent,
+    children: [
+      { path: 'signup', component: SignupComponent },
+      { path: 'signin', component: SigninComponent },
+    ],
   },
 ];
 
