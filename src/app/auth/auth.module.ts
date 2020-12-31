43db598ee7461f6c9743a575efa8ba24e2cb01b2
Jason Yuan <yuanjason98@gmail.com>
2020-12-31 14:17:13 -0500

    added signin comp

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
-  declarations: [AuthComponent, SignupComponent],
+  declarations: [AuthComponent, SignupComponent, SigninComponent],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
