fb93566a42fa3aa2f20d7d86b5fab1acec245bd3
Jason Yuan <yuanjason98@gmail.com>
2021-01-01 16:00:35 -0500

    updated file paths

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from '../app/home/home.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
