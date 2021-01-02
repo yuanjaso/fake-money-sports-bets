bb0d0558512819e3b72666fd01312ea30849d0c4
Jason Yuan <yuanjason98@gmail.com>
2021-01-02 10:13:32 -0500

    added logic to authguard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,15 +2,25 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, first, mapTo, switchMap } from 'rxjs/operators';
+import { MockStoreService } from '../mock-store.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private store: MockStoreService
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -19,6 +29,25 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return true;
+    return this.store.account$.pipe(
+      first(),
+      switchMap((account) => {
+        if (account === undefined) {
+          // try to fetch an account
+          return this.authService.getAccount().pipe(
+            // we just need the request to succeed so if it works then return true
+            mapTo(true),
+            // if the request has an error (most likely 401 unauthenticated) then redirect to signin
+            catchError(() => {
+              this.store.redirectURL = state.url;
+              return of(this.router.parseUrl('/auth/signin'));
+            })
+          );
+        } else {
+          // account exists so return true
+          return of(true);
+        }
+      })
+    );
   }
 }
