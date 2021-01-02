a2a8f4f3da88cb247c6e237e9f0d097ab339b705
Jason Yuan <yuanjason98@gmail.com>
2021-01-02 14:39:19 -0500

    add placeholder nav list inside sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -23,6 +24,7 @@ import { HomeModule } from './home/home.module';
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
+    MatListModule,
     AuthModule,
     HomeModule,
   ],
