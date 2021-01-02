bc26956b6edce9bf37f258bcc0743a47f51a814a
Jason Yuan <yuanjason98@gmail.com>
2021-01-02 13:51:50 -0500

    stick signout button on header/toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  signOut(): void {
+    this.authService.signOut().subscribe();
+    this.router.navigateByUrl('/auth/signin');
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +17,8 @@ import { HomeModule } from './home/home.module';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
     AuthModule,
     HomeModule,
   ],
