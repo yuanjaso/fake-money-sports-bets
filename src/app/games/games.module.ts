6dfdfbcd7d8f9e0e59b1f4b8af7df2663c1a3e46
Jason Yuan <yuanjason98@gmail.com>
2021-01-16 19:12:09 -0500

    added games comp

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { GamesRoutingModule } from './games-routing.module';
+import { GamesComponent } from './games.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [GamesComponent],
   imports: [CommonModule, GamesRoutingModule],
 })
 export class GamesModule {}
