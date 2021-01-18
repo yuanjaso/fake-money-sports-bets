cd5061721b12545190be2e6cd64c9cb2f510d985
Jason Yuan <yuanjason98@gmail.com>
2021-01-18 14:55:09 -0500

    added league tabs for navigation

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { GamesRoutingModule } from './games-routing.module';
 import { GamesComponent } from './games.component';
 
 @NgModule({
   declarations: [GamesComponent],
-  imports: [CommonModule, GamesRoutingModule],
+  imports: [CommonModule, GamesRoutingModule, MatTabsModule],
 })
 export class GamesModule {}
