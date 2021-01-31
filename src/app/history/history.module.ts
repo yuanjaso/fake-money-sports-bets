cbbed3cf873d7a15ef71196ad8aaac2eff154883
Jason Yuan <yuanjason98@gmail.com>
2021-01-31 10:23:19 -0500

    added history feature module

---
+++ src/app/history/history.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HistoryRoutingModule } from './history-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HistoryRoutingModule],
+})
+export class HistoryModule {}
